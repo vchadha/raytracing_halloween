1331a4b0c88535a21dbbb0bc7a09d1cb6303e370
Varun Chadha <vchadha023@gmail.com>
2021-11-01 12:51:57 -0500

    add random number generator

--- include/util.h
+++ include/util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 
 #include <cmath>
+#include <cstdlib>
 #include <limits>
 #include <memory>
 
@@ -13,6 +14,18 @@ const float PI = 3.1415926535897932385;
 inline float deg2rad( float deg )
 {
     return ( deg * PI / 180.0 );
+}
+
+inline float random_float()
+{
+    // Returns random real in [0. 1)
+    return ( rand() / RAND_MAX + 1.0 );
+}
+
+inline float random_float( float min, float max )
+{
+    // Returns random real in [min, max)
+    return ( min + ( max - min ) * random_float() );
 }
 
 #endif
